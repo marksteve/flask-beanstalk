5d7bcbd53b10c8132f33914a591bb2959310a55d
Diego Oliveira <contato@diegoholiveira.com>
2015-09-10 16:52:31 -0300

    Include the files required by setuptools in the build

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
-import os
 from setuptools import setup
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
   setup(
     name='Flask-Beanstalk',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/marksteve/flask-beanstalk',
     license='MIT',
     author='Mark Steve Samson',
     author_email='hello@marksteve.com',
     description='Utilities for using Beanstalk with Flask',
-    long_description=open(os.path.join(current_dir, 'README.rst')).read(),
+    long_description=open('README.rst').read(),
     py_modules=['flask_beanstalk'],
     zip_safe=False,
     platforms='any',
-    install_requires=open(
-      os.path.join(current_dir, 'requirements.txt'),
-    ).readlines(),
+    install_requires=open('requirements.txt').readlines(),
   )
+
